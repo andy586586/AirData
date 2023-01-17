7ad78cefa89f5b06667494b491986a8f430b9a57
andy586586 <111328484+andy586586@users.noreply.github.com>
2023-01-17 16:51:55 -0500

    Update main_table.sql

--- main_table.sql
+++ main_table.sql
@@ -3,7 +3,16 @@ CREATE TABLE Aircraft (
     AircraftName varchar(200),
     ICAO Code varchar(255),
     Maximium Takeoff Weight varchar(255),
-    Max Ramp varchar(255),
+    Max Ramp Max Taxi varchar(255),
     Wake Category varchar(255),
 
 );
+
+
+
+
+
+
+
+INSERT INTO Aircraft (Manufacturer, AircraftName, Icao Code, Maximum Takeoff, Max Ramp Max Taxi, Wake Catergory)
+VALUES (Boeing, 737-100, B731, 1100000, 111000, M);
