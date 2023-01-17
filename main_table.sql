3019afb4426ef14b9780876cf2def8a58759f933
andy586586 <111328484+andy586586@users.noreply.github.com>
2023-01-17 11:39:21 -0500

    Create main_table.sql

---
+++ main_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Aircraft (
+    Manufacturer varchar(200),
+    AircraftName varchar(200),
+    ICAO Code varchar(255),
+    Maximium Takeoff Weight varchar(255),
+    Max Ramp varchar(255),
+    Wake Category varchar(255),
+
+);
